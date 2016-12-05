b49b34723604a728443b8d5caafeac4a4d7aafd9
Steve Kain <stevekain@gmail.com>
2016-12-04 21:12:22 -0600

    Works!

--- js_simulator/sketch.js
+++ js_simulator/sketch.js
@@ -10,41 +10,38 @@ var theColumn = null;
 
 
 
-setup = function() {
+function setup () {
   createCanvas(2000,2000);
-  noStroke();
+  stroke(0);
+  strokeWeight(1);
   midX = width/2;
   midY = height/2;
 
-  theColumn = new ballColumn(0, 0, 100, 36);
-};
+  theColumn = new ballColumn(100, 100, 100, 36);
+}
 
-draw = function() {
+function draw() {
   background(51);
+  for (var i = 0; i < theColumn.balls.length; i++) {
+      var color = getBallColor(i + 1, angle);
+      theColumn.balls[i].setColor(color);
+  }
   theColumn.draw();
-  // drawBall(angle, 1, 100);
-  // drawBall(angle, 2, 100);
-  // drawBall(angle, 3, 100);
-  //
-  //
-  // angle += freq;
-};
+
+  angle += freq;
+}
+
+function getBallColor(ballIndex, angle) {
+    var curBallOffset = ballIndex * ballOffset;
+    var r = colorForAngle(angle * curBallOffset);
+    var g = colorForAngle(angle * curBallOffset * colorOffset);
+    var b = colorForAngle(angle * curBallOffset * colorOffset * 2);
+    return new rgbColor(r, g, b);
+}
 
 
 function colorForAngle(angle) {
   return floor(((255 / 2) * sin(angle)) + (255 / 2));
-}
-
-function drawBall(angle, ballIndex, ballSize) {
-  var curBallOffset = ballIndex * ballOffset;
-  var r = colorForAngle(angle * curBallOffset);
-  var g = colorForAngle(angle * curBallOffset * colorOffset);
-  var b = colorForAngle(angle * curBallOffset * colorOffset * 2);
-  var color = new rgbColor(r, g, b);
-  var the_ball = new ball(20 + ballIndex * ballSize, midY, ballSize, color);
-  the_ball.draw();
-  // fill(r,g,b);
-  // ellipse(20 + ballIndex * ballSize, midY, ballSize, ballSize);
 }
 
 function rgbColor(r,g,b) {
@@ -54,20 +51,22 @@ function rgbColor(r,g,b) {
     self.b = b;
 }
 
-function ball(x, y, _size, _color) {
+function ball(x, y, _size, _color, label) {
     var self = this;
     self.x = x;
     self.y = y;
     self.size = _size;
+    self.label = label;
     self.draw = function() {
         if (self.x > 9000) {
             return;
         }
         fill(self.color.r, self.color.g, self.color.b);
         ellipse(self.x, self.y, self.size, self.size);
-        // console.log('color:');
-        // console.log(self.color);
-        // console.log('pos:' + self.x + ',' + self.y);
+        if (self.label) {
+            fill(0);
+            text(self.label, self.x - 10, self.y - 10);
+        }
     };
 
     self.setColor = function(_color) {
@@ -83,29 +82,35 @@ function ballColumn(x, y, ballSize, numBalls) {
     self.y = y;
     self.balls = [];
     self.numBalls = 0;
+    self.numCols = 6;
+    self.numRows = 6;
 
-
+    //there might be a mathy way of doing this but since I'm just simulating
+    //a 3D object this seemed expedient. Basically the two balls at the
+    //rear are hidden
     self.xposLookup = {
-        0: 99999,
-        1: x,
-        2: x + (ballSize / 2),
-        3: x + ballSize,
-        4: x + ballSize * 2,
+        0: x,
+        1: x + (ballSize / 2),
+        2: x + ballSize * 1.5,
+        3: x + ballSize * 2,
+        4: 99999,
         5: 99999
     };
 
     self.addBall = function() {
-        var mod = self.balls.length === 0 ? 0 : (self.balls.length + 1) % 6;
-        var row = mod + 1;
-        var col = mod;
+        var oldLength = self.balls.length;
+        var col = oldLength % self.numCols;
+        var row = Math.floor((oldLength) / self.numRows);
         var y = ballSize * row + self.y;
         var x = self.xposLookup[col] + self.x;
-        self.balls.push(new ball(x, y, ballSize, new rgbColor(255, 255, 255)));
+        var label = null;
+        // label = '' + (oldLength + 1);
+        self.balls.push(new ball(x, y, ballSize, new rgbColor(255, 255, 255), label));
         self.numBalls = self.balls.length;
     };
 
     self.addBalls = function(numBalls) {
-          for (i = 0; i < numBalls; i++) {
+          for (var i = 0; i < numBalls; i++) {
               self.addBall();
           }
     };
@@ -113,8 +118,12 @@ function ballColumn(x, y, ballSize, numBalls) {
     self.addBalls(numBalls);
 
     self.draw = function () {
-        for (i = 0; i < self.balls.length; i++) {
-            self.balls[i].draw();
+        var order = [0,3,1,2]; //this is more weird 3D->2D funkiness to get the layers right
+        for (var i = 0; i < 6; i ++) {
+            for (var j = 0; j < order.length; j++) {
+                var index = 6 * i + order[j];
+                self.balls[index].draw();
+            }
         }
     };
 }
